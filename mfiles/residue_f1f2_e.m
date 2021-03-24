0fd9dfe042b3cc3b6c4f124ede7d2fa1e812e2a7
gravitional <albertains@163.com>
2021-03-25 04:03:51 +0800

    添加部分计算结果，不计算所有留数

---
+++ mfiles/residue_f1f2_e.m
@@ -0,0 +1,99 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{{-k + p2, MB} -> 
+  {(((-I/4)*CB\[Phi]^2*p1p*Q2*(p1p + p1p*(1 - \[Zeta]))^2*
+       (-1 + y + \[Zeta])^4*(MB^2 - MN^2 + MB*MN*y + MN^2*y - MB^2*\[Zeta] + 
+        MN^2*\[Zeta])*\[CapitalLambda]l^8)/(f^2*Pi^4*(m\[Phi]^2 + MB^2*y - 
+        m\[Phi]^2*y - 2*p1p^2*y + 2*p1p^2*y^2 - m\[Phi]^2*\[Zeta] + 
+        2*p1p^2*y*\[Zeta] + edot[kt, kt] - \[Zeta]*edot[kt, kt] - 
+        2*y*edot[kt, pt] + y*edot[pt, pt])*
+       (MB^2*y - 2*p1p^2*y + 2*p1p^2*y^2 + 2*p1p^2*y*\[Zeta] + 
+         \[CapitalLambda]^2 - y*\[CapitalLambda]^2 - 
+         \[Zeta]*\[CapitalLambda]^2 + edot[kt, kt] - \[Zeta]*edot[kt, kt] - 
+         2*y*edot[kt, pt] + y*edot[pt, pt])^2*
+       (-(MB^2*y) + 2*p1p^2*y - 2*p1p^2*y^2 - MB^2*\[Zeta] + 
+         2*p1p^2*\[Zeta] - 6*p1p^2*y*\[Zeta] + 2*p1p^2*y^2*\[Zeta] - 
+         4*p1p^2*\[Zeta]^2 + 4*p1p^2*y*\[Zeta]^2 + 2*p1p^2*\[Zeta]^3 - 
+         \[CapitalLambda]^2 + y*\[CapitalLambda]^2 + 
+         \[Zeta]*\[CapitalLambda]^2 - edot[kt, kt] + 2*y*edot[kt, pt] + 
+         2*\[Zeta]*edot[kt, pt] - y*edot[pt, pt] - \[Zeta]*edot[pt, pt])^2) - 
+     ((I/8)*CB\[Phi]^2*MN*p1p*(p1p - p1p*(1 - \[Zeta]))^2*
+       (-1 + y + \[Zeta])^4*\[CapitalLambda]l^8*(-8*MB^2*MN + 8*MN^3 - 
+        2*MB*Q2 - 2*MN*Q2 - 8*MB*MN^2*y - 8*MN^3*y - 2*MB*Q2*y - 2*MN*Q2*y + 
+        8*MB^2*MN*\[Zeta] - 2*MB*MN^2*\[Zeta] - 10*MN^3*\[Zeta] + 
+        4*MB*p1p^2*\[Zeta] + 4*MN*p1p^2*\[Zeta] + 3*MB*Q2*\[Zeta] + 
+        3*MN*Q2*\[Zeta] + 4*MB*MN^2*y*\[Zeta] + 4*MN^3*y*\[Zeta] - 
+        8*MB*p1p^2*y*\[Zeta] - 8*MN*p1p^2*y*\[Zeta] + MB*Q2*y*\[Zeta] + 
+        MN*Q2*y*\[Zeta] - 2*MB^2*MN*\[Zeta]^2 + MB*MN^2*\[Zeta]^2 + 
+        3*MN^3*\[Zeta]^2 - 10*MB*p1p^2*\[Zeta]^2 - 10*MN*p1p^2*\[Zeta]^2 - 
+        MB*Q2*\[Zeta]^2 - MN*Q2*\[Zeta]^2 + 8*MB*p1p^2*y*\[Zeta]^2 + 
+        8*MN*p1p^2*y*\[Zeta]^2 + 8*MB*p1p^2*\[Zeta]^3 + 
+        8*MN*p1p^2*\[Zeta]^3 - 2*MB*p1p^2*y*\[Zeta]^3 - 
+        2*MN*p1p^2*y*\[Zeta]^3 - 2*MB*p1p^2*\[Zeta]^4 - 
+        2*MN*p1p^2*\[Zeta]^4 + 4*MB*\[Zeta]*edot[kt, pt] + 
+        4*MN*\[Zeta]*edot[kt, pt] - 2*MB*\[Zeta]^2*edot[kt, pt] - 
+        2*MN*\[Zeta]^2*edot[kt, pt] - 2*MB*\[Zeta]*edot[pt, pt] - 
+        2*MN*\[Zeta]*edot[pt, pt] + MB*\[Zeta]^2*edot[pt, pt] + 
+        MN*\[Zeta]^2*edot[pt, pt]))/(f^2*Pi^4*(m\[Phi]^2 + MB^2*y - 
+        m\[Phi]^2*y - 2*p1p^2*y + 2*p1p^2*y^2 - m\[Phi]^2*\[Zeta] + 
+        2*p1p^2*y*\[Zeta] + edot[kt, kt] - \[Zeta]*edot[kt, kt] - 
+        2*y*edot[kt, pt] + y*edot[pt, pt])*
+       (MB^2*y - 2*p1p^2*y + 2*p1p^2*y^2 + 2*p1p^2*y*\[Zeta] + 
+         \[CapitalLambda]^2 - y*\[CapitalLambda]^2 - 
+         \[Zeta]*\[CapitalLambda]^2 + edot[kt, kt] - \[Zeta]*edot[kt, kt] - 
+         2*y*edot[kt, pt] + y*edot[pt, pt])^2*
+       (-(MB^2*y) + 2*p1p^2*y - 2*p1p^2*y^2 - MB^2*\[Zeta] + 
+         2*p1p^2*\[Zeta] - 6*p1p^2*y*\[Zeta] + 2*p1p^2*y^2*\[Zeta] - 
+         4*p1p^2*\[Zeta]^2 + 4*p1p^2*y*\[Zeta]^2 + 2*p1p^2*\[Zeta]^3 - 
+         \[CapitalLambda]^2 + y*\[CapitalLambda]^2 + 
+         \[Zeta]*\[CapitalLambda]^2 - edot[kt, kt] + 2*y*edot[kt, pt] + 
+         2*\[Zeta]*edot[kt, pt] - y*edot[pt, pt] - \[Zeta]*edot[pt, pt])^2))/
+    (8*(p1p + p1p*(1 - \[Zeta]))^2*(MN^2*(p1p - p1p*(1 - \[Zeta]))^2 - 
+      p1p^2*Q2*(1 - \[Zeta]))), 
+   (((-I/4)*CB\[Phi]^2*MN^2*p1p*(p1p + p1p*(1 - \[Zeta]))^2*
+       (-1 + y + \[Zeta])^4*(MB^2 - MN^2 + MB*MN*y + MN^2*y - MB^2*\[Zeta] + 
+        MN^2*\[Zeta])*\[CapitalLambda]l^8)/(f^2*Pi^4*(m\[Phi]^2 + MB^2*y - 
+        m\[Phi]^2*y - 2*p1p^2*y + 2*p1p^2*y^2 - m\[Phi]^2*\[Zeta] + 
+        2*p1p^2*y*\[Zeta] + edot[kt, kt] - \[Zeta]*edot[kt, kt] - 
+        2*y*edot[kt, pt] + y*edot[pt, pt])*
+       (MB^2*y - 2*p1p^2*y + 2*p1p^2*y^2 + 2*p1p^2*y*\[Zeta] + 
+         \[CapitalLambda]^2 - y*\[CapitalLambda]^2 - 
+         \[Zeta]*\[CapitalLambda]^2 + edot[kt, kt] - \[Zeta]*edot[kt, kt] - 
+         2*y*edot[kt, pt] + y*edot[pt, pt])^2*
+       (-(MB^2*y) + 2*p1p^2*y - 2*p1p^2*y^2 - MB^2*\[Zeta] + 
+         2*p1p^2*\[Zeta] - 6*p1p^2*y*\[Zeta] + 2*p1p^2*y^2*\[Zeta] - 
+         4*p1p^2*\[Zeta]^2 + 4*p1p^2*y*\[Zeta]^2 + 2*p1p^2*\[Zeta]^3 - 
+         \[CapitalLambda]^2 + y*\[CapitalLambda]^2 + 
+         \[Zeta]*\[CapitalLambda]^2 - edot[kt, kt] + 2*y*edot[kt, pt] + 
+         2*\[Zeta]*edot[kt, pt] - y*edot[pt, pt] - \[Zeta]*edot[pt, pt])^2) - 
+     ((I/8)*CB\[Phi]^2*p1p^3*(1 - \[Zeta])*(-1 + y + \[Zeta])^4*
+       \[CapitalLambda]l^8*(-8*MB^2*MN + 8*MN^3 - 2*MB*Q2 - 2*MN*Q2 - 
+        8*MB*MN^2*y - 8*MN^3*y - 2*MB*Q2*y - 2*MN*Q2*y + 8*MB^2*MN*\[Zeta] - 
+        2*MB*MN^2*\[Zeta] - 10*MN^3*\[Zeta] + 4*MB*p1p^2*\[Zeta] + 
+        4*MN*p1p^2*\[Zeta] + 3*MB*Q2*\[Zeta] + 3*MN*Q2*\[Zeta] + 
+        4*MB*MN^2*y*\[Zeta] + 4*MN^3*y*\[Zeta] - 8*MB*p1p^2*y*\[Zeta] - 
+        8*MN*p1p^2*y*\[Zeta] + MB*Q2*y*\[Zeta] + MN*Q2*y*\[Zeta] - 
+        2*MB^2*MN*\[Zeta]^2 + MB*MN^2*\[Zeta]^2 + 3*MN^3*\[Zeta]^2 - 
+        10*MB*p1p^2*\[Zeta]^2 - 10*MN*p1p^2*\[Zeta]^2 - MB*Q2*\[Zeta]^2 - 
+        MN*Q2*\[Zeta]^2 + 8*MB*p1p^2*y*\[Zeta]^2 + 8*MN*p1p^2*y*\[Zeta]^2 + 
+        8*MB*p1p^2*\[Zeta]^3 + 8*MN*p1p^2*\[Zeta]^3 - 
+        2*MB*p1p^2*y*\[Zeta]^3 - 2*MN*p1p^2*y*\[Zeta]^3 - 
+        2*MB*p1p^2*\[Zeta]^4 - 2*MN*p1p^2*\[Zeta]^4 + 
+        4*MB*\[Zeta]*edot[kt, pt] + 4*MN*\[Zeta]*edot[kt, pt] - 
+        2*MB*\[Zeta]^2*edot[kt, pt] - 2*MN*\[Zeta]^2*edot[kt, pt] - 
+        2*MB*\[Zeta]*edot[pt, pt] - 2*MN*\[Zeta]*edot[pt, pt] + 
+        MB*\[Zeta]^2*edot[pt, pt] + MN*\[Zeta]^2*edot[pt, pt]))/
+      (f^2*MN*Pi^4*(m\[Phi]^2 + MB^2*y - m\[Phi]^2*y - 2*p1p^2*y + 
+        2*p1p^2*y^2 - m\[Phi]^2*\[Zeta] + 2*p1p^2*y*\[Zeta] + edot[kt, kt] - 
+        \[Zeta]*edot[kt, kt] - 2*y*edot[kt, pt] + y*edot[pt, pt])*
+       (MB^2*y - 2*p1p^2*y + 2*p1p^2*y^2 + 2*p1p^2*y*\[Zeta] + 
+         \[CapitalLambda]^2 - y*\[CapitalLambda]^2 - 
+         \[Zeta]*\[CapitalLambda]^2 + edot[kt, kt] - \[Zeta]*edot[kt, kt] - 
+         2*y*edot[kt, pt] + y*edot[pt, pt])^2*
+       (-(MB^2*y) + 2*p1p^2*y - 2*p1p^2*y^2 - MB^2*\[Zeta] + 
+         2*p1p^2*\[Zeta] - 6*p1p^2*y*\[Zeta] + 2*p1p^2*y^2*\[Zeta] - 
+         4*p1p^2*\[Zeta]^2 + 4*p1p^2*y*\[Zeta]^2 + 2*p1p^2*\[Zeta]^3 - 
+         \[CapitalLambda]^2 + y*\[CapitalLambda]^2 + 
+         \[Zeta]*\[CapitalLambda]^2 - edot[kt, kt] + 2*y*edot[kt, pt] + 
+         2*\[Zeta]*edot[kt, pt] - y*edot[pt, pt] - \[Zeta]*edot[pt, pt])^2))/
+    (2*(p1p + p1p*(1 - \[Zeta]))^2*(MN^2*(p1p - p1p*(1 - \[Zeta]))^2 - 
+      p1p^2*Q2*(1 - \[Zeta])))}}
